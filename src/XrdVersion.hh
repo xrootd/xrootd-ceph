ad97ff0ed7a7e064b114f93447562b2d7f278ddf
Gerardo Ganis <gerardo.ganis@cern.ch>
2008-02-09 08:04:41 +0000

    Prepare for tag 20071001-0000e

--- src/XrdVersion.hh
+++ src/XrdVersion.hh
@@ -1,7 +1,7 @@
 // $Id$
 #ifndef __XRD_VERSION_H__
 #define __XRD_VERSION_H__
-#define XrdVERSION     "20071001-0000d"
+#define XrdVERSION     "20071001-0000e"
 #if XrdDEBUG
 #define XrdVSTRING XrdVERSION "_dbg"
 #else
