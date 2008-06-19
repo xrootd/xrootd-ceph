af16d9a9110a298e737253cb39438bfc3fcdbbf5
Gerardo Ganis <gerardo.ganis@cern.ch>
2008-06-19 14:15:57 +0000

    Set version to 20071116-0000d for tagging

--- src/XrdVersion.hh
+++ src/XrdVersion.hh
@@ -1,7 +1,7 @@
 // $Id$
 #ifndef __XRD_VERSION_H__
 #define __XRD_VERSION_H__
-#define XrdVERSION     "20071116-0000c"
+#define XrdVERSION     "20071116-0000d"
 #if XrdDEBUG
 #define XrdVSTRING XrdVERSION "_dbg"
 #else
